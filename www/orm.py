21a824538ab6b57fbf0a0be1d7c2c77c96784b7b
yuzeya <578725411@qq.com>
2018-08-28 14:10:39 +0800

    orm test commit

--- www/orm.py
+++ www/orm.py
@@ -35,3 +35,17 @@ def select(sql, args, size=None):
         yield from cur.close()
         logging.info('rows returned: %s' % len(rs))
         return rs
+
+
+@asyncio.coroutine
+def excute(sql, args):
+    log(sql)
+    with (yield from  __pool) as conn:
+        try:
+            cur = yield from conn.cursor()
+            yield from cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            yield from cur.close()
+        except BaseException as e:
+            raise
+        return affected
