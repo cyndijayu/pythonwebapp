b49947ddd94159da2614a7fb9554b276870b8621
yuzeya <578725411@qq.com>
2018-08-29 13:56:57 +0800

    more to commit

--- www/models.py
+++ www/models.py
@@ -10,10 +10,10 @@ class User(Model):
     __table__ = 'users'
 
     id = StringField(primary_key=True, default=next_id(), ddl='varchar(50)')
+    name = StringField(ddl='varchar(50)')
     email = StringField(ddl='varchar(50)')
     passwd = StringField(ddl='varchar(50)')
     admin = BooleanField()
-    name = StringField(ddl='varchar(50)')
     image = StringField(ddl='varchar(500)')
     created_at = FloatField(default=time.time)
 
